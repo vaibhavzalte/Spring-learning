74faae728004fd0b5ef6ded042da356436551b5d
coder_vaibhav <vaibhavzalte1004@gmail.com>
2024-09-16 20:28:04 +0530

    inject collection type list set map properties remains

--- src/main/java/com/uv/myProject/App.java
+++ src/main/java/com/uv/myProject/App.java
@@ -1,23 +1,29 @@
 package com.uv.myProject;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.uv.collections.Emp;
+
 import org.springframework.context.ApplicationContext;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        ApplicationContext context=new ClassPathXmlApplicationContext("config.xml");
-       
+        ApplicationContext context=new ClassPathXmlApplicationContext("config.xml") ;
+
         Student student1=(Student)context.getBean("student1");
         System.out.println(student1);
         Student student2=(Student)context.getBean("student2");
         System.out.println(student2);
 
+        ApplicationContext context2= new ClassPathXmlApplicationContext("com/uv/collections/collectionConfig.xml");
+        Emp emp1=(Emp)context2.getBean("emp1");
+        System.out.println(emp1);
     }
 }
