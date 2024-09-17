03509f9c1ba00a0c27a7e4555453d518504065f3
coder_vaibhav <vaibhavzalte1004@gmail.com>
2024-09-17 17:47:25 +0530

    @value for collections

--- src/main/java/com/uv/stereotype/Student.java
+++ src/main/java/com/uv/stereotype/Student.java
@@ -1,5 +1,7 @@
 package com.uv.stereotype;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,20 @@ public class Student {
 	private String name;
 	@Value("3")
 	private int rollno;
+	@Value("#{mylist}")
+	private List<String> friends;
+	public Student(String name, int rollno, List<String> friends) {
+		super();
+		this.name = name;
+		this.rollno = rollno;
+		this.friends = friends;
+	}
+	public List<String> getFriends() {
+		return friends;
+	}
+	public void setFriends(List<String> friends) {
+		this.friends = friends;
+	}
 	public Student(String name, int rollno) {
 		super();
 		this.name = name;
@@ -23,7 +39,7 @@ public class Student {
 	}
 	@Override
 	public String toString() {
-		return "Student [name=" + name + ", rollno=" + rollno + "]";
+		return "Student [name=" + name + ", rollno=" + rollno + ", friends=" + friends + "]";
 	}
 	public void setName(String name) {
 		this.name = name;
